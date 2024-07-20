8e0a96404df66aa96b96dd81872be49ee97adf19
ishal <muhammedishal0483@gmail.com>
2024-07-20 12:05:39 +0530

    Updated utils

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,3 @@
 module.exports = {
   JWT_KEY: process.env.JWT_KEY,
-  MAILING_PASSWORD: process.env.MAILING_PASSWORD,
-  MAILING_SERVICE: process.env.MAILING_SERVICE,
-  EMAIL: process.env.EMAIL,
 };
